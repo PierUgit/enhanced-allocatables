8048bb96af864984908459487ceef66a2e49b7a6
PierUgit <comptes@ugo235.fr>
2023-11-18 15:49:55 +0100

    comments moved from source to README

--- src/enhanced_allocatables.f90
+++ src/enhanced_allocatables.f90
@@ -1,19 +1,4 @@
 !***********************************************************************************************
-! Extending the allocatable arrays to be dynamically reallocatable/resizable
-! 
-!                          FOR DEMONSTRATION PURPOSE ONLY
-!
-! Limitations
-! - default real kind only
-! - rank-1 only at the moment
-! - size limited to default integer values
-! - size=0 not yet managed
-! - The reallocatable/resizable features are achieved through directly manipulating
-!   the C array descriptors in the framework of the C interoperability: this is non
-!   standard, and therefore not guaranteed to be portable ! 
-!
-!                          FOR DEMONSTRATION PURPOSE ONLY
-!***********************************************************************************************
 MODULE enhanced_allocatables_m
 USE iso_c_binding
 implicit none
