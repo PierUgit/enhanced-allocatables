5f3a31fa767752685d7803bd9fa6c0b8373fc33f
PierUgit <comptes@ugo235.fr>
2023-11-21 15:57:35 +0100

    testing the growth of a normal allocatable

--- src/eatest.f90
+++ src/eatest.f90
@@ -1,9 +1,11 @@
 PROGRAM enhanced_allocatables_p
+USE iso_fortran_env
 USE enhanced_allocatables_m
 implicit none
 
 real, allocatable :: a(:), b(:)
 integer :: i, cap, newcap
+integer(int64) :: location, newlocation
 
 ! Testing C descriptors for different cases of allocatable arrays
 ! unallocated
@@ -39,9 +41,34 @@ print*, lbound(a), size(a), capa(a), a(ubound(a,1))
 
 call edeallocate(a)
 
+print*
+print*, "====== WITH A NORMAL ALLOCATABLE"
+print*, "starts with size=0"
+print*, "iteratively append 1 element, then iteratively drop 1 element"
+allocate(a(0))
+location = loc(a)
+do i = 1, 100000
+   a = [a,real(i)]
+   newlocation = loc(a)
+   if (newlocation /= location) then
+      print*, "size = ", size(a), "   location changed from", location, " to:", newlocation
+      location = newlocation
+   end if
+end do
+do i = 1, 100000
+   a = a(1:size(a)-1)
+   newlocation = loc(a)
+   if (newlocation /= location) then
+      print*, "size = ", size(a), "   location changed from", location, " to:", newlocation
+      location = newlocation
+   end if
+end do
+deallocate(a)
 
-! starts with size=0
-! iteratively append 1 element, then iteratively drop 1 element
+print*
+print*, "====== WITH A ENHANCED ALLOCATABLE"
+print*, "starts with size=0"
+print*, "iteratively append 1 element, then iteratively drop 1 element"
 call eallocate(a,lb=1,ub=0)
 cap = capa(a)
 do i = 1, 100000
@@ -62,13 +89,16 @@ do i = 1, 100000
 end do
 call edeallocate(a)
 
-! start with a single element
-! iteratively append doubles size by self-appending the array, 
-! then iteratively drop half of the elements
+print*
+print*, "====== WITH A ENHANCED ALLOCATABLE"
+print*, "starts with size=1"
+print*, "iteratively triples the size"
+print*, "then iteratively drop 2/3 of the elements"
 call eallocate(a,lb=0,ub=0)
 a(0) = 42.0
 do i = 1, 11
-   call resize(a,keep=.true.,extend=[a,a])
+   call resize(a,lb=1,ub=3*size(a),keep=.true.)
+   a(size(a)/3+1: ) = 42.0
    newcap = capa(a)
    if (newcap /= cap) then
       print*, "size = ", size(a), "   capacity changed from", cap, " to:", newcap
